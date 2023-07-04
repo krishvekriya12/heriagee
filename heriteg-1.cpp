e4b891ec83cb4d14009059edb47fd86b3cc836d0
krish vekriya <131762007+krishvekriya12@users.noreply.github.com>
2023-07-04 15:19:49 +0530

    Add files via upload

---
+++ heriteg-1.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class mother{
+	public :
+		void display()
+		{
+			cout<<"Hoe are you?"<<endl;		
+		}
+};
+class daughter : public mother
+{
+	public :
+		void display()
+		{
+			cout<<"I am fine."<<endl;
+		}
+};
+
+int main()
+{
+    daughter D;
+	
+	D.mother::display();
+	D.display();
+}#include<iostream>
+#include<string.h>
+using namespace std;
+class mother{
+	public :
+		void display()
+		{
+			cout<<"Hoe are you?"<<endl;		
+		}
+};
+class daughter : public mother
+{
+	public :
+		void display()
+		{
+			cout<<"I am fine."<<endl;
+		}
+};
+
+int main()
+{
+    daughter D;
+	
+	D.mother::display();
+	D.display();
+}
